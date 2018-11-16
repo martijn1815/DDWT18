8b5de950b56dde3bec932e233daec1fdfd99527f
Martijn Schendstok <sch.martijn@outlook.com>
2018-11-16 12:28:56 +0100

    added sql database

---
+++ week1/ddwt18_week1.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Gegenereerd op: 16 nov 2018 om 11:16
+-- Serverversie: 5.7.23
+-- PHP-versie: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ddwt18_week1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `series`
+--
+
+CREATE TABLE `series` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `creator` varchar(255) NOT NULL,
+  `seasons` int(11) NOT NULL,
+  `abstract` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `series`
+--
+ALTER TABLE `series`
+  ADD PRIMARY KEY (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
