fe9be318cea375b348b3c37f56c52b3a106227f0
Martijn Schendstok <sch.martijn@outlook.com>
2018-11-18 18:20:28 +0100

    Implemented get_series() and get_series_table() functions.

--- week1/index.php
+++ week1/index.php
@@ -51,32 +51,12 @@ elseif (new_route('/DDWT18/week1/overview/', 'get')) {
         'Add Series' => na('/DDWT18/week1/add/', False)
     ]);
 
-    get_series($db);
     /* Page content */
+    $series = get_series($db);
     $right_column = use_template('cards');
     $page_subtitle = 'The overview of all series';
     $page_content = 'Here you find all series listed on Series Overview.';
-    $left_content = '
-    <table class="table table-hover">
-        <thead>
-        <tr>
-            <th scope="col">Series</th>
-            <th scope="col"></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <th scope="row">House of Cards</th>
-            <td><a href="/DDWT18/week1/serie/" role="button" class="btn btn-primary">More info</a></td>
-        </tr>
-
-        <tr>
-            <th scope="row">Game of Thrones</th>
-            <td><a href="/DDWT18/week1/serie/" role="button" class="btn btn-primary">More info</a></td>
-        </tr>
-
-        </tbody>
-    </table>';
+    $left_content = get_series_table($series);
 
     /* Choose Template */
     include use_template('main');
