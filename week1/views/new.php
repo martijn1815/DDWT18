7b82860bf94c6cc4fb11b7696f429cda94020ca1
Martijn Schendstok <sch.martijn@outlook.com>
2018-11-20 14:00:45 +0100

    Corrected form

--- week1/views/new.php
+++ week1/views/new.php
@@ -34,27 +34,35 @@
                     <h5><?= $page_subtitle ?></h5>
                     <p><?= $page_content ?></p>
                     <!-- Put your form here -->
-                    <div class="form-group row">
-                        <label for="inputName" class="col-sm-2 col-form-label">Name</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputName" name="Name" required>
+                    <form action="<?= $form_action ?>" method="POST">
+                        <div class="form-group row">
+                            <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="inputName" name="Name" required>
+                            </div>
                         </div>
-                        <label for="inputCreator" class="col-sm-2 col-form-label">Creator</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputCreator" name="Creator" required>
+                        <div class="form-group row">
+                            <label for="inputCreator" class="col-sm-2 col-form-label">Creator</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="inputCreator" name="Creator" required>
+                            </div>
                         </div>
-                        <label for="inputSeasons" class="col-sm-2 col-form-label">Seasons</label>
-                        <div class="col-sm-10">
-                            <input type="number" class="form-control" id="inputSeasons" name="Seasons" required>
+                        <div class="form-group row">
+                            <label for="inputSeasons" class="col-sm-2 col-form-label">Seasons</label>
+                            <div class="col-sm-10">
+                                <input type="number" class="form-control" id="inputSeasons" name="Seasons" required>
+                            </div>
                         </div>
-                        <label for="inputAbstract" class="col-sm-2 col-form-label">Abstract</label>
-                        <div class="col-sm-10">
-                            <input type="textarea" class="form-control" id="inputAbstract" name="Abstract" required>
+                        <div class="form-group row">
+                            <label for="inputAbstract" class="col-sm-2 col-form-label">Abstract</label>
+                            <div class="col-sm-10">
+                                <input type="textarea" class="form-control" id="inputAbstract" name="Abstract" required>
+                            </div>
                         </div>
                         <div class="col-sm-10">
                             <button type="submit" class="btn btn-primary"><?= $submit_btn ?></button>
                         </div>
-                    </div>
+                    </form>
                 </div>
 
                 <!-- Right column -->
