ce214176d63643581dba8308c07f15f7e5945f40
Martijn Schendstok <sch.martijn@outlook.com>
2018-11-21 02:52:02 +0100

    final db

--- week1/ddwt18_week1.sql
+++ week1/ddwt18_week1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Gegenereerd op: 20 nov 2018 om 13:41
+-- Gegenereerd op: 21 nov 2018 om 01:50
 -- Serverversie: 5.7.23
 -- PHP-versie: 7.2.10
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `ddwt18_week1`
 --
+CREATE DATABASE IF NOT EXISTS `ddwt18_week1` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ddwt18_week1`;
 
 -- --------------------------------------------------------
 
@@ -40,7 +42,8 @@ CREATE TABLE `series` (
 
 INSERT INTO `series` (`id`, `name`, `creator`, `seasons`, `abstract`) VALUES
 (1, 'House of Cards', 'Beau Willimon', 6, 'A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him.'),
-(2, 'Game of Thrones', 'David Benioff', 7, 'Game of Thrones is an American fantasy drama television series created by David Benioff and D. B. Weiss.');
+(2, 'Game of Thrones', 'David Benioff', 7, 'Game of Thrones is an American fantasy drama television series created by David Benioff and D. B. Weiss.'),
+(3, 'How I Met Your Mother', 'Carter Bays', 9, 'The series concerns the adventures of Ted Mosby (played by Josh Radnor) narrating the story of how he met the mother of his children. ');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -60,7 +63,7 @@ ALTER TABLE `series`
 -- AUTO_INCREMENT voor een tabel `series`
 --
 ALTER TABLE `series`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
