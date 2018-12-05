ecafe8bc5d78779c9e7f5f4262af134844e8fb60
Martijn Schendstok <sch.martijn@outlook.com>
2018-12-05 14:23:48 +0100

    Database with values

--- week3/ddwt18_week3.sql
+++ week3/ddwt18_week3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Gegenereerd op: 05 dec 2018 om 13:04
+-- Gegenereerd op: 05 dec 2018 om 13:23
 -- Serverversie: 5.7.23
 -- PHP-versie: 7.2.10
 
@@ -37,6 +37,14 @@ CREATE TABLE `series` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Gegevens worden geëxporteerd voor tabel `series`
+--
+
+INSERT INTO `series` (`id`, `name`, `creator`, `seasons`, `abstract`) VALUES
+(1, 'House of Cards', 'Beau Willimon', 6, 'A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him.'),
+(2, 'Game of Thrones', 'David Benioff', 7, 'Game of Thrones is an American fantasy drama television series created by David Benioff and D. B. Weiss.');
+
+--
 -- Indexen voor geëxporteerde tabellen
 --
 
@@ -54,7 +62,7 @@ ALTER TABLE `series`
 -- AUTO_INCREMENT voor een tabel `series`
 --
 ALTER TABLE `series`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
